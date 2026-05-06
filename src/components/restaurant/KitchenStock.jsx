fa80447ae82fcf26874369330b7f8868af65b0b3
FRANCIS007 <francisnujimem@gmail.com>
2026-05-07 00:52:58 +0100

    update

--- src/components/restaurant/KitchenStock.jsx
+++ src/components/restaurant/KitchenStock.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axiosWithAuth from "../../utils/axiosWithAuth";
-import "./KitchenBalanceStock.css";
+import "./KitchenStock.css";
 
 const KitchenBalanceStock = () => {
   const [balances, setBalances] = useState([]);
